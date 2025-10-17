419c661714e8cc173a0243fef5859e7ce233e02b
jespernyman <jnyman21@student.oulu.fi>
2025-10-17 20:32:23 +0300

    fix url issue

--- fetch_devs.py
+++ fetch_devs.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 import git
 from config import *
 import itertools
+import shutil
 
 # This block of code take the repository, fetches all the commits,
 # retrieves name and email of both the author and commiter and saves the unique
@@ -15,15 +16,18 @@ import itertools
 LIMIT = 5000  # Limit how many commits are fetched
 
 # Count commits for cool progress bar
-repo = git.Repo(REPO_PATH)
-totalcommits = int(repo.git.rev_list('--count', 'HEAD'))
- 
+if os.path.isdir(REPO_PATH):
+    repo = git.Repo(REPO_PATH)
+    totalcommits = int(repo.git.rev_list('--count', 'HEAD'))
+    display_total = min(totalcommits, LIMIT) if LIMIT else totalcommits
+else:
+    display_total = None
+
 DEVS = set()
 commits_iter = Repository(REPO_PATH).traverse_commits()
 if LIMIT:
     commits_iter = itertools.islice(commits_iter, LIMIT)
 
-display_total = min(totalcommits, LIMIT) if LIMIT else totalcommits
 for commit in tqdm(commits_iter, total=display_total, desc="Processing commits"):
     # Check that both the author and committer have emails and names
     if commit.author.name and commit.author.email:
