c7f7840034fe853c57bf6de82ba1b7d727bb5205
gitjussi <jussi2001@gmail.com>
2025-11-02 15:54:41 +0200

    csv_compare.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-REPO_PATH = "https://github.com/LibreOffice/core"  # Put repo path (or url) here
+REPO_PATH = "E:\GitHubRepot\core"  # Put repo path (or url) here
 TEAM_MEMBER = "JussiS" # Put your name here
