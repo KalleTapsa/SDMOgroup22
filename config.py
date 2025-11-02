d506a6c4c8f640ff32d6554cbe8c7cf3f27c835f
Jussi <71126344+jussisaari@users.noreply.github.com>
2025-11-02 22:17:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-REPO_PATH = "E:\GitHubRepot\core"  # Put repo path (or url) here
-TEAM_MEMBER = "jussis"  # Put your name here
+REPO_PATH = "path"  # Put repo path (or url) here
+TEAM_MEMBER = "name"  # Put your name here
